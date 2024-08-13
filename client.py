1896354cb7e0221984be3a4b85033c5d88d2168d
Leo Lee <leosungwonlee@gmail.com>
2024-08-13 13:35:20 -0400

    print data in terminal

--- client.py
+++ client.py
@@ -2,9 +2,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+
 from supabase import create_client
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 
 supabase = create_client(url, key)
+
+data = supabase.table("players_test").select("first_name, last_name").execute()
+print(data)
